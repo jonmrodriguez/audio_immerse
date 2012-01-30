15ccaf9991b4e59500ea70e7d6b6a5782ef3f459
Jon Rodriguez <luciddreamworld@gmail.com>
2012-01-30 03:21:11 -0800

    switch to the fast x tracker

--- immersive_audio.py
+++ immersive_audio.py
@@ -15,6 +15,6 @@ import os
 # second arg is loop delay between face detections (in seconds)
 # next four args are left x, right x, top y, bottom y. left-right from cam's POV
 # last arg is the outputting callback
-os.system('track_faces 0 .05 1. -1. 1. -1. volume_balance')
+os.system('track_one_face_x 0 .05 1. -1. 1. -1. volume_balance')
 
 
