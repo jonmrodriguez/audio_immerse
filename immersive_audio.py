4250223eac216b6a40321f702f15ab16dbb48d73
Jon Rodriguez <luciddreamworld@gmail.com>
2012-01-30 02:02:02 -0800

    fix documentation error. make loop faster

--- immersive_audio.py
+++ immersive_audio.py
@@ -12,8 +12,9 @@ immersive_audio
 import os
 
 # first arg is camera id. 0 means iSight webcam
+# second arg is loop delay between face detections (in seconds)
 # next four args are left x, right x, top y, bottom y. left-right from cam's POV
 # last arg is the outputting callback
-os.system('track_faces 0 .1 1. -1. 1. -1. volume_balance')
+os.system('track_faces 0 .05 1. -1. 1. -1. volume_balance')
 
 
